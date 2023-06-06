f388039a05694332f1fd50b7c24cb7ab30a20495
Yonathan Uchôa Simão Kaveski <yonathan.uchoa@gmail.com>
2023-06-06 06:08:34 -0300

    feat: match new file names from model

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,12 @@
 const associations = async (schemas) => {
-  schemas["User"].hasMany(schemas["Task"], { foreignKey: "id_user" }); // user/task association
+  schemas["User"].hasMany(schemas["Task"], { foreignKey: "id", as: "id_user" }); // user/task association
   await schemas["Task"].sync({ alter: true }); // sync tasks table to inser association
 };
 
 module.exports = {
   schemas: {
-    User: require("./users"),
-    Task: require("./tasks"),
+    User: require("./users-model"),
+    Task: require("./tasks-model"),
   },
   associations,
 };
